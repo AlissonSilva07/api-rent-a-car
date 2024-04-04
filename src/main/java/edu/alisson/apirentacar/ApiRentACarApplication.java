680af1c2ec4701d096e42c6f221cccdd0331c15a
Alisson Rego <alissondroid07@gmail.com>
2024-04-04 14:24:15 -0300

    commit inicial

---
+++ src/main/java/edu/alisson/apirentacar/ApiRentACarApplication.java
@@ -0,0 +1,13 @@
+package edu.alisson.apirentacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRentACarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRentACarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/alisson/apirentacar/ApiRentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.alisson.apirentacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRentACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
